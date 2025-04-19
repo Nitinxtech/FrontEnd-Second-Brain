ebdafb1f4cfea70cf7ff26a69dd0f4887840c5c0
Nitinxtech <nitinp3006@gmail.com>
2025-04-19 19:28:13 +0530

    first button component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/Button'
+import { PlusIcon } from './icons/PlusIcon'
+
+function App() {
+  return (
+    <>
+    <Button startIcon={<PlusIcon/>} size="sm" variant="primary" text="Share"/>
+    <Button size="md" variant="secondary" text="Add Content"/>
+    <Button size="lg" variant="secondary" text="Add Content"/>
+    </>
+  )
+}
+
+export default App
